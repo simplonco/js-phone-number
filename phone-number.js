2f1a0bb1dde00d4d76b557c63ee20cd294e04132
adora84 <abdulkader.kalaaji@gmail.com>
2016-04-27 11:28:08 +0200

    test1

--- phone-number.js
+++ phone-number.js
@@ -7,16 +7,26 @@
 'use strict';
 
 var Phone = module.exports = function Phone(number) {
-  this.rawNumber = number;
-  this.cleanedNumber = this.cleanNumber(number);
+    this.rawNumber = number;
+    this.cleanedNumber = this.cleanNumber(number);
 };
 
 Phone.prototype.cleanNumber = function(number) {
 //
 // YOUR CODE GOES HERE
 //
-};
+    var num = number.replace(/\D/g, '');
 
+    if (num.length < 10 || num.length > 12) {
+        return console.log("bad number");
+    } else if (num.length = 11 && num[0] !== 1) {
+        return console.log("bad number");
+    } else if (number.length = 11 && num[0] === 1) {
+        return num.substr(1, num.length);
+    } else {
+        return num;
+    }
+};
 Phone.prototype.number = function() {
-  return this.cleanedNumber;
+    return this.cleanedNumber;
 };
